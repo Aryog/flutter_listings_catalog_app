0c886a592bcd66cccae6b68c69c10f6b5939bdec
Aryog <yogesh.772417@thc.tu.edu.np>
2023-02-02 21:11:28 +0545

    list vew of catalog app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:catalog_application/pages/home_page.dart';
+import 'package:catalog_application/pages/login_page.dart';
+import 'package:catalog_application/utils/routes.dart';
+import 'package:catalog_application/widgets/themes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage()
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:catalog_application/models/catalog.dart';
+import 'package:catalog_application/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+import '../widgets/item_widget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData['products'];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: CatalogModel.items.length,
+                  itemBuilder: (context, index) {
+                    return ItemWidget(item: CatalogModel.items[index]);
+                  })
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
+      drawer: MyDrawer(),
+    );
+  }
+}
